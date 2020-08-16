3d2a9d8373381aca09639516d9e18cbc8963bf35
Pablo Onate Villaverde <pablo.onate.villaverde@gmail.com>
2020-08-16 19:46:55 +0200

    Added Docs

---
+++ docs/precache-manifest.4a1a5163f410c42a0a9bd0f06259cd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01f44fd9d205a2ccd3915d712418629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95721e79a3c76c9a7867",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95721e79a3c76c9a7867",
+    "url": "/static/js/main.60f04f54.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
